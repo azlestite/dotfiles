d4cec171bd18547a69e55dd15f5eb49c7125e33e
azlestite <34531033+azlestite@users.noreply.github.com>
2018-08-18 17:21:48 +0900

    feat(*): add install script for cmake

---
+++ etc/install_cmake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Install cmake
+
+set -Ceu -o pipefail
+
+cd $HOME/local/src
+version=3.12
+build=1
+pkg=cmake-${version}.${build}
+wget -qO - https://cmake.org/files/v${version}/${pkg}.tar.gz | tar xvfz -
+cd ${pkg}
+./configure --prefix=$HOME/local
+make -j4
+make install

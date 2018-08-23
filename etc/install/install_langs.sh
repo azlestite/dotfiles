073a6b5183cb8298f9ac7f6892abf2ceaa35c69a
azlestite <34531033+azlestite@users.noreply.github.com>
2018-08-23 22:20:09 +0900

    feat(*): add install script for python, nodejs and go

---
+++ etc/install/install_langs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Install python, nodejs, and go
+
+set -Ceu -o pipefail
+
+echo "--------- Python -----------------------"
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+  libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev
+pyenv --version
+pyenv install -l
+python3_latest=$(pyenv install -l | grep -v '[a-z]' | tail -1 | sed 's/\s//g')
+python2_latest=$(pyenv install -l | grep -v '[a-z]' | grep '2.7.[0-9]*' | tail -1 | sed 's/\s//g')
+pyenv install $python3_latest
+pyenv install $python2_latest
+pyenv global $python3_latest
+pyenv rehash
+python --version
+pip install --upgrade pip
+pip install pipenv
+echo "--------- END --------------------------"
+
+echo "--------- Node.js ----------------------"
+nodenv --version
+nodenv install -l
+node_latest=$(nodenv install -l | grep '^\s*[0-9].*' | tail -1 | sed 's/\s//g')
+nodenv install $node_latest
+nodenv global $node_latest
+nodenv rehash
+node -v
+npm -v
+echo "--------- END --------------------------"
+
+echo "--------- Go ----------------------"
+goenv --version
+goenv install -l
+go_latest=$(goenv install -l | grep -v '[a-z]' | tail -1 | sed 's/\s//g')
+goenv install $go_latest
+goenv global $go_latest
+goenv rehash
+go version
+echo "--------- END --------------------------"
+
+exec $SHELL -l

1d37fcbff8e40f4b1a23faf93b6e8f070a6ad802
azlestite <34531033+azlestite@users.noreply.github.com>
2018-08-24 22:34:17 +0900

    feat(*): add setup script to create symbolic links

---
+++ etc/link.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Create symbolic links
+
+set -Ceu -o pipefail
+
+source ${script_dir:-$(cd $(dirname ${BASH_SOURCE}) && pwd)}/util/include.sh
+
+echo ""
+info "Create symbolic links..."
+echo ""
+
+cd $dot_dir
+
+array=(".config" ".gnupg" ".ssh" ".tmux")
+
+for f in .??*
+do
+  [[ "$f" == ".git" ]] && continue
+
+  for str in "${array[@]}"
+  do
+    [[ "$f" == "${str}" ]] && continue 2
+  done
+
+  symlink "$dot_dir/$f" "$HOME/$f"
+  echo "$f"
+
+  if [ $? -eq 0 ]; then
+    printf "  %-25s -> %s\n" "\$dotfiles/$f" "\$HOME/$f"
+  fi
+done
+
+for d in "${array[@]}"
+do
+  sym_dot_dir "$d"
+done

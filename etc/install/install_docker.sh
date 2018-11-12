392a84e6cf0079d65637540c08ca8210df903505
azlestite <34531033+azlestite@users.noreply.github.com>
2018-11-13 00:05:39 +0900

    feat(*): add install script for docker and docker compose

--- etc/install/install_docker.sh
+++ etc/install/install_docker.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 #
 # Install Docker
-# WSL doesn't support MS_SLAVE yet, so docker after 17.09.1 get error.
 
 set -Ceu -o pipefail
 
-cd $HOME/local/src
-curl -O https://download.docker.com/linux/debian/dists/stretch/pool/stable/amd64/docker-ce_17.09.0~ce-0~debian_amd64.deb
-sudo dpkg -i docker-ce_17.09.0~ce-0~debian_amd64.deb
-sudo cgroupfs-mount
-sudo usermod -aG docker $USER
-sudo service docker start
-echo "Run WSL as administrator and run next command."
-echo "sudo cgroupfs-mount && sudo service docker start"
+sudo apt install apt-transport-https \
+  ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Fingerprint should be 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt update
+
+sudo apt install docker-ce
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod a+rx /usr/local/bin/docker-compose
+
+docker-compose --version
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+sudo chmod a+r /etc/bash_completion.d/docker-compose
+source /etc/bash_completion.d/docker-compose

427421a1d8461611ec4b5a0ad470e0dd21a99fa0
azlestite <34531033+azlestite@users.noreply.github.com>
2019-12-29 13:28:37 +0900

    fix script

--- etc/install/install_neovim.sh
+++ etc/install/install_neovim.sh
@@ -5,11 +5,11 @@
 set -Ceu -o pipefail
 
 sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt update -q
 sudo apt install -y neovim
 nvim --version
-sudo apt install -y python-dev python-pip python3-dev python3-pip
+#sudo apt install -y python-dev python-pip python3-dev python3-pip
 
 # cpsm
-sudo apt install cmake libboost-all-dev libicu-dev
+#sudo apt install cmake libboost-all-dev libicu-dev

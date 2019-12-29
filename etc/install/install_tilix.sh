3aeed66fea5bf3745f3ac38e03fb87bde1ba68e2
azlestite <34531033+azlestite@users.noreply.github.com>
2019-12-29 23:30:45 +0900

    fix script

--- etc/install/install_tilix.sh
+++ etc/install/install_tilix.sh
@@ -6,20 +6,20 @@ set -Ceu -o pipefail
 
 sudo add-apt-repository -y ppa:webupd8team/terminix
 sudo apt update
-sudo apt install tilix
+sudo apt install -y tilix
 
 # dbus-x11が無いとPreferenceを設定できなかった
-sudo apt install dbus-x11
+sudo apt install -y dbus-x11 x11-xserver-utils
 
 # copy Myrica font from windows
 sudo mkdir -p /usr/share/fonts/truetype/myrica
-sudo cp /mnt/c/Windows/Fonts/Myrica* /usr/share/fonts/truetype/myrica/
+sudo cp /mnt/d/PTools/Fonts/Myrica*.ttf /usr/share/fonts/truetype/myrica/
 sudo fc-cache -fv
 
 # TilixのカラースキームをMolokaiにするためダウンロード
 wget -qO $HOME"/.config/tilix/schemes/molokai.json" https://git.io/v7QVE
 
 # fcitx-mozcのインストール
-sudo apt install fcitx-mozc
+sudo apt install -y fcitx-mozc
 # machine-idに生成したUUIDを書き込む
 sudo sh -c "dbus-uuidgen > /var/lib/dbus/machine-id"

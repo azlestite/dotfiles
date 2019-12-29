31c8c25a509f9f1f757c6b904e26966d404e62b6
azlestite <34531033+azlestite@users.noreply.github.com>
2019-12-29 14:14:12 +0900

    fix script

--- etc/install/install_envs.sh
+++ etc/install/install_envs.sh
@@ -5,26 +5,26 @@
 set -Ceu -o pipefail
 
 script_dir="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
-anyenv_bin=$HOME/.anyenv/bin/anyenv
+#anyenv_bin=$HOME/.anyenv/bin/anyenv
 
 echo "--------- anyenv -----------------------"
 git clone https://github.com/anyenv/anyenv $HOME/.anyenv
 #exec $SHELL -l
 source ~/.profile # .bashrc内部でanyenv init -は実行される。
 #eval "$($any_bin init -)"
-echo y | $anyenv_bin install --init
-#echo y | anyenv install --init
+#echo y | $anyenv_bin install --init
+echo y | anyenv install --init
 
-#any_root="$(anyenv root)"
-any_root="$($anyenv_bin root)"
+any_root="$(anyenv root)"
+#any_root="$($anyenv_bin root)"
 mkdir -p ${any_root}/plugins
 git clone https://github.com/znz/anyenv-update.git ${any_root}/plugins/anyenv-update
 git clone https://github.com/znz/anyenv-git.git ${any_root}/plugins/anyenv-git
 echo "--------- END --------------------------"
 
 echo "--------- *envs ------------------------"
-$anyenv_bin install pyenv
-#anyenv install pyenv
+#$anyenv_bin install pyenv
+anyenv install pyenv
 #anyenv install nodenv
 #anyenv install goenv
 
@@ -33,4 +33,4 @@ $anyenv_bin install pyenv
 echo "--------- END --------------------------"
 
 #source ./etc/install/install_langs.sh
-source $script_dir/install_langs.sh
+#source $script_dir/install_langs.sh

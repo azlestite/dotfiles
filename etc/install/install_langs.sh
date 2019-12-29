0542ebcb5eb37563ecf099cb30b65d4d8255b8d5
azlestite <34531033+azlestite@users.noreply.github.com>
2019-12-29 22:54:44 +0900

    fix

--- etc/install/install_langs.sh
+++ etc/install/install_langs.sh
@@ -8,24 +8,21 @@ echo "--------- Python for NeoVim -----------------------"
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
   libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev
 pyenv --version
-#source ~/.profile
-pyenv install -l
-pyenv --help
-#python2_latest=$(pyenv install -l | grep -v '[a-z]' | grep '2.7.[0-9]*' | tail -1 | sed 's/\s//g')
-#python3_latest=$(pyenv install -l | grep -v '[a-z]' | tail -1 | sed 's/\s//g')
-#echo "Install python ${python2_latest}..."    
-#pyenv install $python2_latest
-#echo "Install python ${python3_latest}..."    
-#pyenv install $python3_latest
-#pyenv rehash
-#pyenv global $python2_latest
-#pyenv which python
-#pip install pynvim
-#pyenv global $python3_latest
-#pyenv which python
-#python --version
-#pip install --upgrade pip
-#pip install pipenv pynvim
+python2_latest=$(pyenv install -l | grep -v '[a-z]' | grep '2.7.[0-9]*' | tail -1 | sed 's/\s//g')
+python3_latest=$(pyenv install -l | grep -v '[a-z]' | tail -1 | sed 's/\s//g')
+echo "Install python ${python2_latest}..."    
+pyenv install $python2_latest
+echo "Install python ${python3_latest}..."    
+pyenv install $python3_latest
+pyenv rehash
+pyenv global $python2_latest
+pyenv which python
+pip install pynvim
+pyenv global $python3_latest
+pyenv which python
+python --version
+pip install --upgrade pip
+pip install pipenv pynvim
 echo "--------- END --------------------------"
 
 #echo "--------- Node.js ----------------------"

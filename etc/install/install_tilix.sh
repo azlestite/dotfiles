109de1a090bbbf91ea0672a0eddd8675cb89c52e
azlestite <34531033+azlestite@users.noreply.github.com>
2019-12-30 11:40:09 +0900

    fix

--- etc/install/install_tilix.sh
+++ etc/install/install_tilix.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 #
-# Setup Tilix
+# Setup Tilix Terminal
 
 set -Ceu -o pipefail
 
+# Tilix
 sudo add-apt-repository -y ppa:webupd8team/terminix
-sudo apt update
-sudo apt install -y tilix
+sudo apt update -qq
+sudo apt install -y tilix dbus-x11 x11-xserver-utils
 # prevent "bash: /etc/profile.d/vte.sh: No such file or directory"
 sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
-# dbus-x11が無いとPreferenceを設定できなかった
-sudo apt install -y dbus-x11 x11-xserver-utils
-
-# fcitx-mozcのインストール
+# Fcitx and Mozc
 sudo apt install -y fcitx-mozc
-# machine-idに生成したUUIDを書き込む
+# write UUID to machine-id
 sudo sh -c "dbus-uuidgen > /var/lib/dbus/machine-id"
 
-# Copy Myrica font from windows
+# Myrica font
 sudo mkdir -p /usr/share/fonts/truetype/myrica
 sudo cp /mnt/d/PTools/Fonts/Myrica*.ttf /usr/share/fonts/truetype/myrica/
 sudo fc-cache -fv
 
-# Download Molokai color scheme for Tilix
+# Molokai color scheme for Tilix
 mkdir -p ~/.config/tilix/schemes
 wget -qO $HOME"/.config/tilix/schemes/molokai.json" https://git.io/v7QVE
 
-# Install GTK Theme
+# Install Adapta GTK Theme
 sudo add-apt-repository -y ppa:tista/adapta
-sudo apt update -q
+sudo apt update -qq
 sudo apt install -y adapta-gtk-theme

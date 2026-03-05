62d754b39295a66a10c738c3c33a6498fbc70468
azlestite <34531033+azlestite@users.noreply.github.com>
2026-03-05 16:37:40 +0900

    fix: modify not found error

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 # exit immediately if password-manager-binary is already in $PATH
-type bitwarden >/dev/null 2>&1 && exit
+type bw >/dev/null 2>&1 && exit
 
 case "$(uname -s)" in
 Linux)
     # commands to install password-manager-binary on Linux
     echo "Install Bitwarden CLI..."
     if [ "$(uname -m)" = "x86_64" ]; then
+        sudo apt install -y unzip
         version=2026.1.0
+        mkdir -p $HOME/.local/{bin,src}
         curl -Lo $HOME/.local/src/bw-linux.zip https://github.com/bitwarden/clients/releases/download/cli-v${version}/bw-linux-${version}.zip
         unzip $HOME/.local/src/bw-linux.zip -d $HOME/.local/bin/
         chmod +x $HOME/.local/bin/bw

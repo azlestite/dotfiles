14f9e98b1e4bcb9d14376c513b72785c3f8e0632
azlestite <34531033+azlestite@users.noreply.github.com>
2026-05-08 22:51:19 +0900

    wip

--- install/ubuntu/chezmoi_private.sh
+++ install/ubuntu/chezmoi_private.sh
@@ -42,11 +42,11 @@ function uninstall_chezmoi_private() {
 function main() {
   # source ~/.bashrc
 
-  # # Use gpg-agent instead of ssh-agent
-  # if ! pgrep -x -u "${USER:-$(whoami)}" gpg-agent >/dev/null 2>&1; then
-  #   echo "Starting gpg-agent..."
-  #   gpg-connect-agent /bye >/dev/null 2>&1
-  # fi
+  # Use gpg-agent instead of ssh-agent
+  if ! pgrep -x -u "${USER:-$(whoami)}" gpg-agent >/dev/null 2>&1; then
+    echo "Starting gpg-agent..."
+    gpg-connect-agent /bye >/dev/null 2>&1
+  fi
 
   unset SSH_AGENT_PID
   echo "gnupg_SSH_AUTH_SOCK_by: ${gnupg_SSH_AUTH_SOCK_by:-0}"

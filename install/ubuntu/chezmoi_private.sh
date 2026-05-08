484105fea3ff49a8077b5a1735c5d0dd619bba21
azlestite <34531033+azlestite@users.noreply.github.com>
2026-05-08 22:56:30 +0900

    wip

--- install/ubuntu/chezmoi_private.sh
+++ install/ubuntu/chezmoi_private.sh
@@ -48,12 +48,12 @@ function main() {
     gpg-connect-agent /bye >/dev/null 2>&1
   fi
 
-  # unset SSH_AGENT_PID
-  # echo "gnupg_SSH_AUTH_SOCK_by: ${gnupg_SSH_AUTH_SOCK_by:-0}"
-  # if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  #   SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-  #   export SSH_AUTH_SOCK
-  # fi
+  unset SSH_AGENT_PID
+  echo "gnupg_SSH_AUTH_SOCK_by: ${gnupg_SSH_AUTH_SOCK_by:-0}"
+  if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    export SSH_AUTH_SOCK
+  fi
 
   GPG_TTY=$(tty)
   export GPG_TTY
